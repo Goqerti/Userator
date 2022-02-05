26295d6225fe84f630205bf38c332722bdd259bd
Ümüd Məmmədov <98206328+uumud@users.noreply.github.com>
2022-02-05 12:44:08 +0400

    Update userator.py

--- userbot/modules/userator.py
+++ userbot/modules/userator.py
@@ -7,5 +7,5 @@ from userbot.events import register
 @register(incoming=True, from_users=UPBOT, pattern="/ualive$")
 @register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.ualive$")
 @register(incoming=True, from_users=WHITELIST, pattern="^.ualive$")
-async def sudoers(s):
-    await s.client.send_message(s.chat_id,f"**Bot işləyir ✅**\n`U S Σ R Δ T O R ✨` **versiya:** {DTO_VERSION}")
+async def _(q):
+    await q.client.send_message(q.chat_id,"`U S Σ R Δ T O R ✨ Online`")
