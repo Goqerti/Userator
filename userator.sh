5d0f0df2af374709eadf31544973b2b56ca36812
Ümüd Məmmədov <98206328+uumud@users.noreply.github.com>
2022-03-06 17:18:17 +0400

    U S Σ R Δ T O R

---
+++ userator.sh
@@ -0,0 +1,12 @@
+pkg install python
+clear
+pkg install python3
+clear
+pkg update
+clear
+git clone https://github.com/uumud/Qurulum
+clear
+cd up_qurulum
+clear
+pip install -r requirements.txt
+python3 -m up_qurulum && exit
