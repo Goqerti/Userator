709280b3718f4cc7324c52bc570807d26d1a2254
Ümüd Məmmədov <98206328+uumud@users.noreply.github.com>
2022-03-06 18:30:43 +0400

    U S Σ R Δ T O R

---
+++ up.sh
@@ -0,0 +1,48 @@
+Userator+="\nU S Σ R Δ T O R ✨"
+MESAJ+="\n "
+MESAJ+="\n✅Kanal: @UseratorOT"
+MESAJ+="\n✅Qrup: @UseratorSup"
+MESAJ+="\n "
+KOMEK+="\n "
+BOSLUQ="\n "
+clear
+echo -e $BOSLUQ
+echo -e $BOSLUQ
+pkg update -y && pkg upgrade
+clear
+echo -e $BOSLUQ
+apt upgrade -y
+echo -e $BOSLUQ
+echo -e $MESAJ
+echo -e $BOSLUQ
+echo "Python ✅"
+echo -e $BOSLUK
+pkg install python3
+pip3 install --upgrade pip
+clear
+echo -e $MESAJ
+echo -e $BOSLUQ
+echo "Git ✅"
+echo -e $BOSLUQ
+pkg install git -y
+clear
+echo -e $MESAJ
+echo -e $BOSLUQ
+echo "Telethon ✅"
+echo -e $BOSLUQ
+pip install telethon
+clear
+echo -e $MESAJ
+echo -e $BOSLUQ
+echo "Repo ✅"
+echo -e $BOSLUQ
+rm -rf Qurulum
+git clone https://github.com/uumud/Qurulum
+clear
+echo -e $BOSLUQ
+echo -e $MESAJ
+echo -e $BOSLUQ
+echo -e $BOSLUQ
+cd Qurulum
+pip install -r requirements.txt
+python -m up_installer
